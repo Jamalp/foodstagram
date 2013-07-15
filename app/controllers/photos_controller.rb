4b0e7a13e8280af13d0cf11275ee03b286a40151
Jamal Powell <jamalkpowell@gmail.com>
2013-07-15 19:03:30 -0400

    fixing heroku

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,7 +3,7 @@ class PhotosController < ApplicationController
   end
 
   def result
-    client = Foursquare2::Client.new(:client_id => ENV['foursquare_client_id'], :client_secret => ENV['foursquare_client_secret'])
+    client = Foursquare2::Client.new(:client_id => ENV['FOURSQUARE_CLIENT_ID'], :client_secret => ENV['FOURSQUARE_CLIENT_SECRET'])
     if params[:location]
       @search = client.search_venues(:near => params[:location], :query =>  params[:place])
       @foursquare_id = @search.groups[0][:items].first[:id]

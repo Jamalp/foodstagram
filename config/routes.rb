0f2deb5d401c8c0de2053ed3192b9639237f26db
Jamal Powell <jamalkpowell@gmail.com>
2013-07-17 20:51:18 -0400

    fixed thumbnails

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Foodstagram::Application.routes.draw do
   devise_for :users
 
   root:to => 'photos#home', as: 'photo_home'
+  get '/user/:id' => 'photos#index', as: 'photo_user'
   get '/photos' => 'photos#result', as: 'photo_result'
   get '/about' => 'photos#about', as: 'photo_about'
   post '/photos' => 'photos#save', :as => 'save_place'
